bb2c0b38fe74ca866704784ff4d17b515eb29bc2
Jonathan Wilkinson <jonathan.wilkinson@dyson.com>
2016-11-28 09:14:00 +0000

    Fix for bug #18

--- WanikaniBurnReviews.user.js
+++ WanikaniBurnReviews.user.js
@@ -2,7 +2,7 @@
 // @name        Wanikani Burn Reviews
 // @namespace   wkburnreviewnew
 // @description Adds a space on the main page that reviews random burned items. This is a maintained fork of the original script by Samuel Harbord
-// @version     2.2.0
+// @version     2.2.1
 // @author      Jonny Dark
 // @license     Creative Commons Attribution-NonCommercial 4.0 International (CC BY-NC 4.0); http://creativecommons.org/licenses/by-nc/4.0/
 // @include     http://www.wanikani.com/
@@ -161,7 +161,7 @@ function addBurnReviewStylesThen(callback) {
             for (var i = 0; i < links.length; i++)
             {
                 var link = links[i];
-                if (link.type == "text/css")
+                if (link.href.endsWith(".css"))
                 {
                     BRLog("Adding " + link.outerHTML + " to document head");
                     $("head").append(link);
